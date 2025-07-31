144ab3dc468634affe823438ad13a708db62533c
chadgptcont <reece@rmitechnical.com>
2025-07-31 00:17:20 -0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "RMI Services LLC";
+export const SITE_DESCRIPTION = "Home theater and automation for the Willamette Valley";
